d0cb8e6bc48298f3a518b3bc086c2e1ef1c17124
Adam Wawrzeńczyk <adam.wawrzenczyk@gmail.com>
2022-09-28 13:31:07 +0200

    Add project files.

---
+++ QuickTag/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace QuickTag.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
